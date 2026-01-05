cfe992601f82f5768b27e462bc8fbcd717f0f0a7
s354774 <s354774@studenti.polito.it>
2026-01-05 19:43:07 +0100

    fix: minor fix

--- Source/button_EXINT/IRQ_button.c
+++ Source/button_EXINT/IRQ_button.c
@@ -19,16 +19,13 @@ void EINT1_IRQHandler (void)	  	/* KEY1														 */
 {	
 	NVIC_DisableIRQ(EINT1_IRQn);		/* disable Button interrupts			 */
 	LPC_PINCON->PINSEL4    &= ~(1 << 22);     /* GPIO pin selection */
-	
-	LPC_GPIO2->FIODIR &= ~(1 << 11); /* forzo la direzione di lettura come INPUT */
-
 	down=1;
-	LPC_SC->EXTINT &= (1 << 0);     /* clear pending interrupt         */
+	LPC_SC->EXTINT &= (1 << 1);     /* clear pending interrupt         */
 }
 
 void EINT2_IRQHandler (void)	  	/* KEY2														 */
 {
-	LPC_SC->EXTINT &= (1 << 0);     /* clear pending interrupt         */
+	LPC_SC->EXTINT &= (1 << 2);     /* clear pending interrupt         */
 }
 
 
