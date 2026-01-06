08b87f82cb03d8826638bbddd6639b359e4bc1c4
Giuseppe Calvello <calvello71@gmail.com>
2026-01-06 22:18:46 +0100

    fix: added LPC_GPIO2->FIODIR &= ~(1 << 12); to IRQ_button

--- Source/button_EXINT/IRQ_button.c
+++ Source/button_EXINT/IRQ_button.c
@@ -28,6 +28,7 @@ void EINT2_IRQHandler (void)	  	/* KEY2														 */
 {
 	NVIC_DisableIRQ(EINT2_IRQn);		/* disable Button interrupts			 */
 	LPC_PINCON->PINSEL4    &= ~(1 << 24);     /* GPIO pin selection */
+	LPC_GPIO2->FIODIR &= ~(1 << 12); // aggiunta di sicurezza
 	down2=1;
 	LPC_SC->EXTINT &= (1 << 2);     /* clear pending interrupt         */
 }
